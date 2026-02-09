3ef1be6084341089d8a921cde80e04a724e056b4
Ahmed-2020-98 <ahmedgamalmansour11@gmail.com>
2026-02-09 19:56:55 +0300

    CSRF token mismatch solved

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -19,8 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
         ]);
-
-        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
